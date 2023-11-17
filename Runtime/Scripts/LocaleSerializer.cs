8a4a76f3704ec8feb867ba4d9d5eb7f5ef941393
unknown <drunk3n.p1nky@gmail.com>
2023-11-17 21:31:24 +0500

    Renaming of TXTLoader into LocaleSerializer

---
+++ Runtime/Scripts/LocaleSerializer.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Pinky.Localization.Containers;
+
+namespace Pinky.Localization
+{
+    public static class LocaleSerializer
+    {
+        public static SystemLanguage DefaultLanguage = SystemLanguage.English;
+        private static LocalesTextContainer s_localesTextContainer;
+
+        public static byte[] Serialize(Dictionary<string, string> localizationMap)
+        {
+            return MessagePack.MessagePackSerializer.Serialize(localizationMap);
+        }
+
+        public static Dictionary<string, string> Deserialize(SystemLanguage targetLanguage)
+        {
+            if (!s_localesTextContainer)
+            {
+                Debug.LogError($"{nameof(s_localesTextContainer)} is null. Check Resources folder.");
+                return new Dictionary<string, string>();
+            }
+
+            bool isSuccess = s_localesTextContainer.TryGetLocalizationFile(targetLanguage, out TextAsset binaryFile);
+
+            if (!isSuccess)
+                s_localesTextContainer.TryGetLocalizationFile(DefaultLanguage, out binaryFile);
+
+           return Deserialize(binaryFile);
+        }
+
+        public static Dictionary<string, string> Deserialize(SystemLanguage targetLanguage, LocalesTextContainer localesTextContainer) 
+        {
+            s_localesTextContainer = localesTextContainer;
+            return Deserialize(targetLanguage);
+        }
+
+        public static Dictionary<string, string> Deserialize(TextAsset binaryFile)
+        {
+            Dictionary<string, string> localizationMap = MessagePack.MessagePackSerializer.Deserialize<Dictionary<string, string>>(binaryFile.bytes);
+            return localizationMap;
+        }
+
+        public static Dictionary<SystemLanguage, Dictionary<string, string>> DeserializeLocales()
+        {
+            Dictionary<SystemLanguage, Dictionary<string, string>> allLocales = new();
+
+#if UNITY_EDITOR
+            if (!s_localesTextContainer)
+                Initialize();
+#endif
+
+            SystemLanguage[] systemLanguages = s_localesTextContainer.GetKeys();
+
+            for (int i = 0; i < systemLanguages.Length; i++)
+            {
+                SystemLanguage currentLanguage = systemLanguages[i];
+                s_localesTextContainer.TryGetLocalizationFile(currentLanguage, out TextAsset file);
+
+                if (!file)
+                    continue;
+
+                allLocales.Add(currentLanguage, Deserialize(file));
+            }
+
+            return allLocales;
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        private static void Initialize()
+        {
+            s_localesTextContainer = Resources.Load<LocalesTextContainer>("Localization/Locales Text Container");
+        }
+    }
+}
